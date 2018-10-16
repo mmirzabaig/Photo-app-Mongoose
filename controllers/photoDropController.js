ead6c3c2738355ae426559c102b85708403073e7
Mirza Baig <mirzabaig@Mirzas-MacBook-Pro.local>
2018-10-16 10:21:06 -0500

    index and new page working

--- controllers/photoDropController.js
+++ controllers/photoDropController.js
@@ -5,7 +5,24 @@ const router = express.Router();
 const photoDrop = require('../models/photoDrop');
 
 
+router.get('/', (req, res) => {
+  photoDrop.find({}, (err, photosFound) => {
+    res.render('./photoDrop/index.ejs', {
+      photos: photosFound
+    });
+  });
+});
 
+router.get('/new', (req, res) => {
+  res.render('./photoDrop/new.ejs');
+})
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  photoDrop.create(req.body, (err, photoFound) => {
+    res.redirect('/photos');
+  });
+});
 
 
 
