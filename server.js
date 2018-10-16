5b61992d3302424de5143ab8d00fd5a916b7783f
Mirza Baig <mirzabaig@Mirzas-MacBook-Pro.local>
2018-10-15 21:42:38 -0500

    first

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+require('./db/db');
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+})
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+  console.log('YOUR APP IS LIVE!')
+})
